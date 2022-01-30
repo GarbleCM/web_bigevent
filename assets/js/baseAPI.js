c4d19f5438c279317afe8c79cd44c48651ba7fa7
GarbleCM <1010311620@qq.com>
2022-01-30 18:20:44 +0800

    完成主页功能

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,4 +1,22 @@
 $.ajaxPrefilter(function(options){
     options.url = 'http://www.liulongbin.top:3007' + options.url;
     
+    /* 为要权限的请求统一设置请求体headers */
+    if(options.url.indexOf('/my/') !== -1){
+        options.headers = {
+            Authorization : localStorage.getItem('token') || '',
+        }
+        
+    }
+
+    // 全局挂载complete 配置对象
+    options.complete = function(res){
+        console.log(res);
+        if(res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！'){
+            // 清空缓存token
+            localStorage.removeItem('token');
+            // 返回登录页面
+            location.href = '/login.html'
+        } 
+    }
 })
---
+++ assets/js/index.js
@@ -0,0 +1,49 @@
+$(function(){
+    getUserInfo();
+    const layer = layui.layer;
+    $('#btnLogOut').on('click', function(e){
+        layer.confirm('确认退出登录?', {icon: 3, title:'提示'}, function(index){
+            // 清除本地缓存中的token
+            localStorage.removeItem('token')
+            // 重新跳转到登录页面
+            location.href = '/login.html'
+            // 关闭confirm弹出层
+            layer.close(index);
+        });
+    })
+})
+function getUserInfo(){
+    $.ajax({
+        method: 'GET',
+        url: '/my/userinfo',
+        /* headers:{
+            Authorization: localStorage.getItem('token') 
+        }, */
+        success: function(res){
+            if(res.status !== 0){
+                return layui.layer.msg(res.message);
+            }
+            console.log(res);
+            renderAvatar(res.data);
+        },    
+    })
+}
+
+/* 渲染用户头像 */
+function renderAvatar(data){
+    // 1. 获取用户的名称
+    var name = data.nickname || data.username;
+    // 2. 渲染用户名
+    $('#welcome').html('欢迎&nbsp;&nbsp;' + name)
+    // 3. 按需渲染用户头像
+    if(data.user_pic !== null){
+        // 3.1 优先渲染图片头像
+        $('.layui-nav-img').attr('src',data.user_pic).show();
+        $('.user-avatar').hide();
+    }else{
+        // 3.2 无图片头像渲染文本头像
+        $('.layui-nav-img').hide();
+        var first = name[0].toUpperCase();
+        $('.user-avatar').html(first).show()
+    }
+}
